e1b2048c2a09c9cccb2d61aee275521fc75d4bd9
Colin MacDonald <colin@goinstant.com>
2014-04-30 15:46:23 -0300

    travis deploy

---
+++ deploy/deploy.js
@@ -0,0 +1,27 @@
+/* jshint node:true */
+
+'use strict';
+
+var config = require('./config.json');
+var S3Deploy = require('./lib/s3_deploy');
+
+function deploy() {
+  var s3Deploy = new S3Deploy(config);
+
+  s3Deploy.deploy(function(err, success) {
+    if (err) {
+      return console.log('deploy failed!');
+    }
+
+    if (success) {
+      console.log('deploy successful!');
+
+    } else {
+      console.log('no deploy');
+    }
+  });
+}
+
+deploy();
+
+
